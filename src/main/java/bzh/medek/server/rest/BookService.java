3ebe04567f3d83f89266311b6393ceaf69bd5ede
msansm1 <msansm1@dizalch.fr>
2015-04-19 10:35:55 +0200

    REST => book authors load.sql => add artist types, artists, artist book, album & movie

--- src/main/java/bzh/medek/server/rest/BookService.java
+++ src/main/java/bzh/medek/server/rest/BookService.java
@@ -33,152 +33,181 @@ import bzh.medek.server.persistence.entities.Book;
 @Produces(MediaType.APPLICATION_JSON)
 public class BookService extends Application {
 
-    private static final Logger LOGGER = Logger.getLogger(BookService.class);
-    
-    @Inject
-    BookDAO bookDao;
-    @Inject
-    BooktypeDAO booktypeDAO;
-    @Inject
-    StorygenreDAO storygenreDAO;
-    @Inject
-    EditorDAO editorDAO;
-    @Inject
-    CollectionDAO collectionDAO;
-    @Inject
-    LangDAO langDAO;
-	
-	public BookService () {
+	private static final Logger LOGGER = Logger.getLogger(BookService.class);
+
+	@Inject
+	BookDAO bookDao;
+	@Inject
+	BooktypeDAO booktypeDAO;
+	@Inject
+	StorygenreDAO storygenreDAO;
+	@Inject
+	EditorDAO editorDAO;
+	@Inject
+	CollectionDAO collectionDAO;
+	@Inject
+	LangDAO langDAO;
+
+	public BookService() {
 	}
 
-    /**
-     *  GET /books : retrieve all books
-     * 
-     * @return
-     */
-    @GET
-    public List<JsonBook> getAll() {
-    	List<Book> books = bookDao.getBooks();
-    	LOGGER.info("find "+books.size()+" books in the database");
-    	ArrayList<JsonBook> lb = new ArrayList<JsonBook>();
-    	for (Book b : books) {
-    		lb.add(new JsonBook(b.getId(), b.getTitle(), "", 0, 
-    				(b.getEditorBean() != null)?b.getEditorBean().getName():"",
-    				(b.getEditorBean() != null)?b.getEditorBean().getId():0,
-    				(b.getCollectionBean() != null)?b.getCollectionBean().getName():"",
-    				(b.getCollectionBean() != null)?b.getCollectionBean().getId():0,
-    				b.getCover(), b.getDescription(), b.getPublicationdate(), 
-    				(b.getStorygenre() != null)?b.getStorygenre().getName():"",
-    				(b.getStorygenre() != null)?b.getStorygenre().getId():0, 
-    				(b.getBooktype() != null)?b.getBooktype().getName():"",
-    				(b.getBooktype() != null)?b.getBooktype().getId():0,
-    				(b.getBooktype() != null)?b.getLangBean().getName():"",
-    				(b.getBooktype() != null)?b.getLangBean().getId():0, b.getSeries(), 
-    				b.getBooknb(), b.getIsseriedone()));
-    	}
-    	return lb;
-    }
+	/**
+	 * GET /books : retrieve all books
+	 * 
+	 * @return
+	 */
+	@GET
+	public List<JsonBook> getAll() {
+		List<Book> books = bookDao.getBooks();
+		LOGGER.info("find " + books.size() + " books in the database");
+		ArrayList<JsonBook> lb = new ArrayList<JsonBook>();
+		String artistName = "";
+		Integer artistId = 0;
+		for (Book b : books) {
+			if (!b.getBookartists().isEmpty()) {
+				artistName = b.getBookartists().get(0).getArtistBean()
+						.getName()
+						+ " "
+						+ b.getBookartists().get(0).getArtistBean()
+								.getFirstname();
+				artistId = b.getBookartists().get(0).getArtistBean().getId();
+			} else {
+				artistName = "";
+				artistId = 0;
+			}
+			lb.add(new JsonBook(b.getId(), b.getTitle(), artistName, artistId, (b
+					.getEditorBean() != null) ? b.getEditorBean().getName()
+					: "", (b.getEditorBean() != null) ? b.getEditorBean()
+					.getId() : 0, (b.getCollectionBean() != null) ? b
+					.getCollectionBean().getName() : "",
+					(b.getCollectionBean() != null) ? b.getCollectionBean()
+							.getId() : 0, b.getCover(), b.getDescription(), b
+							.getPublicationdate(),
+					(b.getStorygenre() != null) ? b.getStorygenre().getName()
+							: "", (b.getStorygenre() != null) ? b
+							.getStorygenre().getId() : 0,
+					(b.getBooktype() != null) ? b.getBooktype().getName() : "",
+					(b.getBooktype() != null) ? b.getBooktype().getId() : 0, (b
+							.getBooktype() != null) ? b.getLangBean().getName()
+							: "", (b.getBooktype() != null) ? b.getLangBean()
+							.getId() : 0, b.getSeries(), b.getBooknb(), b
+							.getIsseriedone()));
+		}
+		return lb;
+	}
 
-    /**
-     *  GET /books/{id} : retrieve one book
-     * 
-     * @param id
-     * @return
-     */
-    @GET
-    @Path(value = "/{id}")
-    public JsonBook getOne(@PathParam(value = "id") Integer id) {
-    	Book b = bookDao.getBook(id);
-    	LOGGER.info("find "+b.getTitle()+" book in the database");
-    	return new JsonBook(b.getId(), b.getTitle(), "", 0, 
-				(b.getEditorBean() != null)?b.getEditorBean().getName():"",
-				(b.getEditorBean() != null)?b.getEditorBean().getId():0,
-				(b.getCollectionBean() != null)?b.getCollectionBean().getName():"",
-				(b.getCollectionBean() != null)?b.getCollectionBean().getId():0,
-				b.getCover(), b.getDescription(), b.getPublicationdate(), 
-				(b.getStorygenre() != null)?b.getStorygenre().getName():"",
-				(b.getStorygenre() != null)?b.getStorygenre().getId():0, 
-				(b.getBooktype() != null)?b.getBooktype().getName():"",
-				(b.getBooktype() != null)?b.getBooktype().getId():0,
-				(b.getBooktype() != null)?b.getLangBean().getName():"",
-				(b.getBooktype() != null)?b.getLangBean().getId():0, b.getSeries(), 
-				b.getBooknb(), b.getIsseriedone());
-    }
+	/**
+	 * GET /books/{id} : retrieve one book
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@GET
+	@Path(value = "/{id}")
+	public JsonBook getOne(@PathParam(value = "id") Integer id) {
+		Book b = bookDao.getBook(id);
+		LOGGER.info("find " + b.getTitle() + " book in the database");
+		String artistName = "";
+		Integer artistId = 0;
+		if (!b.getBookartists().isEmpty()) {
+			artistName = b.getBookartists().get(0).getArtistBean().getName()
+					+ " "
+					+ b.getBookartists().get(0).getArtistBean().getFirstname();
+			artistId = b.getBookartists().get(0).getArtistBean().getId();
+		}
+		return new JsonBook(b.getId(), b.getTitle(), artistName, artistId,
+				(b.getEditorBean() != null) ? b.getEditorBean().getName() : "",
+				(b.getEditorBean() != null) ? b.getEditorBean().getId() : 0,
+				(b.getCollectionBean() != null) ? b.getCollectionBean()
+						.getName() : "", (b.getCollectionBean() != null) ? b
+						.getCollectionBean().getId() : 0, b.getCover(),
+				b.getDescription(), b.getPublicationdate(),
+				(b.getStorygenre() != null) ? b.getStorygenre().getName() : "",
+				(b.getStorygenre() != null) ? b.getStorygenre().getId() : 0,
+				(b.getBooktype() != null) ? b.getBooktype().getName() : "",
+				(b.getBooktype() != null) ? b.getBooktype().getId() : 0,
+				(b.getBooktype() != null) ? b.getLangBean().getName() : "",
+				(b.getBooktype() != null) ? b.getLangBean().getId() : 0,
+				b.getSeries(), b.getBooknb(), b.getIsseriedone());
+	}
 
-    /**
-     *  POST /movies : create / update one movie
-     * 
-     * @param id
-     * @return
-     */
-    @POST
-    public JsonBook createUpdateOne(JsonBook jb) {
-    	JsonBook jbook = jb;
-    	if (jb.getId() == null) {
-    		Book b = new Book();
-    		b.setTitle(jb.getTitle());
-    		b.setBooknb(jb.getBookNb());
-    		b.setDescription(jb.getDescription());
-    		b.setCover(jb.getCover());
-    		b.setIsseriedone(jb.getIsSerieDone());
-    		b.setSeries(jb.getSeries());
-    		b.setPublicationdate(jb.getPublicationDate());
-    		if (jb.getEditorId() != null) {
-    			b.setEditorBean(editorDAO.getEditor(jb.getEditorId()));
-    		}
-    		if (jb.getCollectionId() != null) {
-    			b.setCollectionBean(collectionDAO.getCollection(jb.getCollectionId()));
-    		}
-    		if (jb.getLangId() != null) {
-    			b.setLangBean(langDAO.getLang(jb.getLangId()));
-    		}
-    		if (jb.getTypeId() != null) {
-    			b.setBooktype(booktypeDAO.getBooktype(jb.getTypeId()));
-    		}
-    		if (jb.getGenreId() != null) {
-    			b.setStorygenre(storygenreDAO.getStorygenre(jb.getGenreId()));
-    		}
-    		bookDao.saveBook(b);
-	    	jbook.setId(b.getId());
-    	} else {
-    		Book b = bookDao.getBook(jb.getId());
-    		b.setTitle(jb.getTitle());
-    		b.setBooknb(jb.getBookNb());
-    		b.setDescription(jb.getDescription());
-    		b.setCover(jb.getCover());
-    		b.setIsseriedone(jb.getIsSerieDone());
-    		b.setSeries(jb.getSeries());
-    		b.setPublicationdate(jb.getPublicationDate());
-    		if (jb.getEditorId() != null) {
-    			b.setEditorBean(editorDAO.getEditor(jb.getEditorId()));
-    		}
-    		if (jb.getCollectionId() != null) {
-    			b.setCollectionBean(collectionDAO.getCollection(jb.getCollectionId()));
-    		}
-    		if (jb.getLangId() != null) {
-    			b.setLangBean(langDAO.getLang(jb.getLangId()));
-    		}
-    		if (jb.getTypeId() != null) {
-    			b.setBooktype(booktypeDAO.getBooktype(jb.getTypeId()));
-    		}
-    		if (jb.getGenreId() != null) {
-    			b.setStorygenre(storygenreDAO.getStorygenre(jb.getGenreId()));
-    		}
-    		bookDao.updateBook(b);
-    	}
-    	return jbook;
-    }
+	/**
+	 * POST /movies : create / update one movie
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@POST
+	public JsonBook createUpdateOne(JsonBook jb) {
+		JsonBook jbook = jb;
+		if (jb.getId() == null) {
+			Book b = new Book();
+			b.setTitle(jb.getTitle());
+			b.setBooknb(jb.getBookNb());
+			b.setDescription(jb.getDescription());
+			b.setCover(jb.getCover());
+			b.setIsseriedone(jb.getIsSerieDone());
+			b.setSeries(jb.getSeries());
+			b.setPublicationdate(jb.getPublicationDate());
+			if (jb.getEditorId() != null) {
+				b.setEditorBean(editorDAO.getEditor(jb.getEditorId()));
+			}
+			if (jb.getCollectionId() != null) {
+				b.setCollectionBean(collectionDAO.getCollection(jb
+						.getCollectionId()));
+			}
+			if (jb.getLangId() != null) {
+				b.setLangBean(langDAO.getLang(jb.getLangId()));
+			}
+			if (jb.getTypeId() != null) {
+				b.setBooktype(booktypeDAO.getBooktype(jb.getTypeId()));
+			}
+			if (jb.getGenreId() != null) {
+				b.setStorygenre(storygenreDAO.getStorygenre(jb.getGenreId()));
+			}
+			bookDao.saveBook(b);
+			jbook.setId(b.getId());
+		} else {
+			Book b = bookDao.getBook(jb.getId());
+			b.setTitle(jb.getTitle());
+			b.setBooknb(jb.getBookNb());
+			b.setDescription(jb.getDescription());
+			b.setCover(jb.getCover());
+			b.setIsseriedone(jb.getIsSerieDone());
+			b.setSeries(jb.getSeries());
+			b.setPublicationdate(jb.getPublicationDate());
+			if (jb.getEditorId() != null) {
+				b.setEditorBean(editorDAO.getEditor(jb.getEditorId()));
+			}
+			if (jb.getCollectionId() != null) {
+				b.setCollectionBean(collectionDAO.getCollection(jb
+						.getCollectionId()));
+			}
+			if (jb.getLangId() != null) {
+				b.setLangBean(langDAO.getLang(jb.getLangId()));
+			}
+			if (jb.getTypeId() != null) {
+				b.setBooktype(booktypeDAO.getBooktype(jb.getTypeId()));
+			}
+			if (jb.getGenreId() != null) {
+				b.setStorygenre(storygenreDAO.getStorygenre(jb.getGenreId()));
+			}
+			bookDao.updateBook(b);
+		}
+		return jbook;
+	}
 
-    /**
-     *  GET /books/user/{id} : retrieve books for one user
-     * 
-     * @param id - user ID
-     * @return
-     */
-    @GET
-    @Path(value = "/user/{id}")
-    public List<JsonBook> getUserBooks(@PathParam(value = "id") Integer id) {
-    	return bookDao.getUsersBooks(id);
-    }
-    
+	/**
+	 * GET /books/user/{id} : retrieve books for one user
+	 * 
+	 * @param id
+	 *            - user ID
+	 * @return
+	 */
+	@GET
+	@Path(value = "/user/{id}")
+	public List<JsonBook> getUserBooks(@PathParam(value = "id") Integer id) {
+		return bookDao.getUsersBooks(id);
+	}
+
 }
