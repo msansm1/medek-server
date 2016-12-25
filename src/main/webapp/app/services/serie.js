0c5c89d70a582cafac790534a3ac29a3ec1ad1df
msansm1 <msansm1@dizalch.fr>
2016-12-25 18:51:53 +0100

    client services cleaning

--- src/main/webapp/app/services/serie.js
+++ src/main/webapp/app/services/serie.js
@@ -2,16 +2,16 @@ angular.module('medekApp.services').service('SerieService', [
 '$http', 
 '$rootScope', 
 function($http, $rootScope) {
-    this.series = function() {
-        return $http.get('services/tvshows/loguser/'+$rootScope.user.id);
-    };
-
     this.serieslist = function(from, limit, orderBy, orderDir) {
         return $http.get('services/tvshows?from='+from+'&limit='+limit+'&orderBy='+orderBy+'&orderDir='+orderDir);
     };
+
+    this.userSerieslist = function(from, limit, orderBy, orderDir, userId) {
+        return $http.get('services/tvshows/user?from='+from+'&limit='+limit+'&orderBy='+orderBy+'&orderDir='+orderDir+'&userId='+userId);
+    };
     
     this.serie = function(id) {
-        return $http.get('services/tvshows/'+id+'/loguser/'+$rootScope.user.id);
+        return $http.get('services/tvshows/'+id);
     };
     
     this.saveSerie = function(serie) {
@@ -24,9 +24,5 @@ function($http, $rootScope) {
     
     this.addSerieToCollection = function(serie) {
         return $http.post('services/tvshows/addtocollec', serie);
-    };
-
-    this.userSerieslist = function(from, limit, orderBy, orderDir, userId) {
-        return $http.get('services/tvshows/user?from='+from+'&limit='+limit+'&orderBy='+orderBy+'&orderDir='+orderDir+'&userId='+userId);
     };
 } ]);
